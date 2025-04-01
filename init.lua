d04d35cd7028e30c5ba41fce40d4884b92bb60d6
Aiden Asprakis <aasprakis@mac.lan>
2025-03-31 23:19:12 -0400

    Add closing brackets plugin

--- init.lua
+++ init.lua
@@ -997,6 +997,14 @@ require('lazy').setup({
   require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    config = true,
+    -- use opts = {} for passing setup options
+    -- this is equivalent to setup({}) function
+  },
+
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
