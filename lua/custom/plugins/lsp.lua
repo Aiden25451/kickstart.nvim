5c50ab4058b6e9f5edcbd2bb953e82f9251185fb
Aiden Asprakis <aasprakis@mac.lan>
2025-04-27 21:02:15 -0400

    Underline everything now

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -154,7 +154,14 @@ return {
     vim.diagnostic.config {
       severity_sort = true,
       float = { border = 'rounded', source = 'if_many' },
-      underline = { severity = vim.diagnostic.severity.ERROR },
+      underline = {
+        severity = {
+          vim.diagnostic.severity.HINT,
+          vim.diagnostic.severity.WARN,
+          vim.diagnostic.severity.INFO,
+          vim.diagnostic.severity.ERROR,
+        },
+      },
       signs = vim.g.have_nerd_font and {
         text = {
           [vim.diagnostic.severity.ERROR] = '󰅚 ',
@@ -164,14 +171,12 @@ return {
         },
       } or {},
       virtual_text = {
-        source = 'if_many',
-        spacing = 2,
         format = function(diagnostic)
           local diagnostic_message = {
-            [vim.diagnostic.severity.ERROR] = diagnostic.message,
-            [vim.diagnostic.severity.WARN] = diagnostic.message,
-            [vim.diagnostic.severity.INFO] = diagnostic.message,
-            [vim.diagnostic.severity.HINT] = diagnostic.message,
+            [vim.diagnostic.severity.ERROR] = '󰅚 ',
+            [vim.diagnostic.severity.WARN] = '󰀪 ',
+            [vim.diagnostic.severity.INFO] = '󰋽 ',
+            [vim.diagnostic.severity.HINT] = '󰌶 ',
           }
           return diagnostic_message[diagnostic.severity]
         end,
